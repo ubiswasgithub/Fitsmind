0afc7d37d43ea7db5e0694dea6bbe61bc88b9621
ubiswasgithub <uttamcsedu@gmail.com>
2018-01-23 15:54:24 +0600

    Add files via upload
    added updated configuration file

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ exports.config ={
 	suites:{
 		preloggedhomepage: 'tests/preloggedhomespec.js',
 		registrationpage: 'tests/registrationpagespec.js',
-		loginpage:'tests/loginpagespec.js'
+		loginpage:'tests/loginpagespec.js',
+		logoutpage: 'tests/logoutpagespec.js'
 	},
 	//specs:'tests/registrationpagespec.js'	
 	capabilities: {
